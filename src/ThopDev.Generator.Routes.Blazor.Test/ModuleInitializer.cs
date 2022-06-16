180cb524d4f91ec976b6c625e4a4b159c8aefe09
THop <github@thop.dev>
2022-06-16 20:38:36 +0200

    module init

---
+++ src/ThopDev.Generator.Routes.Blazor.Test/ModuleInitializer.cs
@@ -0,0 +1,13 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace ThopDev.Generator.Routes.Test;
+
+public class ModuleInitializer
+{
+    [ModuleInitializer]
+    public static void Initialize()
+    {
+        VerifierSettings.ScrubInlineGuids();
+        VerifierSettings.ScrubEmptyLines();
+    }
+}
